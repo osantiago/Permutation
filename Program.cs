e614ebb018b79c3e8130f65fc0c6b117ed318f52
osantiago <orlando.santiago193@yahoo.com>
2019-02-08 21:43:19 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IsPermutation
+{
+    class Program
+    {
+        static bool IsPermutation(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            foreach (var item in s1)
+            {
+                if (!s2.Contains(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsPermutation("acdf", "dfac"));
+        }
+    }
+}
